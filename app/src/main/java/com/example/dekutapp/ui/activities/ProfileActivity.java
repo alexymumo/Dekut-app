92e90f3bd5a04020da5a8dd930bccfe05be42d39
Alex-mumo <alexymumo2000@gmail.com>
2021-10-12 10:13:33 +0300

    update

--- app/src/main/java/com/example/dekutapp/ui/activities/ProfileActivity.java
+++ app/src/main/java/com/example/dekutapp/ui/activities/ProfileActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.dekutapp.R;
+import com.example.dekutapp.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
+    private ActivityProfileBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
